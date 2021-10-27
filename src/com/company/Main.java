ab7fd26b9be18d8271e0e5889d4c537509cce834
OHpetro <petiamatiu9@gmail.com>
2021-10-27 18:45:41 +0300

    ver 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,71 +25,7 @@ public class Main {
             StringBuilder sb = new StringBuilder();
             StringBuilder in = new StringBuilder();
 
-            try {
-                BufferedReader br = new BufferedReader(new FileReader(csv_rpath));
-
-                while((line = br.readLine()) != null){
-
-                    String[] value = line.split("(,|;)(?=(?:[^“]*“[^“]*“)*[^“]*$)");
-
-                    for(int i = 0; i < value.length; i++) {
-
-                        if(value[i]==""){
-                            continue;
-                        }
-
-
-                        if(value[i].indexOf("“") == 0){
-                            value[i] = value[i].replaceFirst("“","");
-
-                            if(value[i].contains("““") == true){
-                                value[i] = value[i].substring(0,value[i].length()-1);
-                                value[i] = value[i].replaceFirst("““","“");
-                            }else if(value[i].indexOf("“") != value[i].length()-1){
-                                value[i] = value[i].replaceFirst("“","");
-                            }else{
-                                value[i] = value[i].replace(value[i].substring(value[i].length()-1),"");
-                            }
-
-
-                        }else if(value[i].contains("““") == true){
-
-                            value[i] = value[i].replaceFirst("““","“");
-
-                        }else if(value[i].indexOf("“") != 0 && value[i].contains("“") == true){
-                            String[] dopp = value[i].split(",");
-                            for(int a=0;a<dopp.length;a++){
-                                sb.append(dopp[a]+" ");
-                                in.append(dopp[a].length()+"+");
-                            }
-                            continue;
-
-
-                        }else{
-                            value[i] =value[i];
-                        }
-
-                        sb.append(value[i]+" ");
-                        in.append(value[i].length()+"+");
-
-                    }
-
-                    sb.append(in);
-                    sb.deleteCharAt(sb.lastIndexOf("+"));
-                    sb.append("\n");
-                    in.delete(0, in.length());
-                }
-                System.out.println(sb.toString());
-                pw.write(sb.toString());
-                pw.close();
-
-
-
-            }catch (FileNotFoundException e){
-                e.printStackTrace();
-            }catch (IOException e){
-                e.printStackTrace();
-            }
+            Stringdestroyer.stringdestroyer(csv_rpath,pw,sb,in);
 
 
         }catch(FileNotFoundException e){
--- src/com/company/Stringdestroyer.java
+++ src/com/company/Stringdestroyer.java
@@ -1,11 +1,78 @@
 package com.company;
+import java.io.PrintWriter;
+import java.io.*;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.File;
 
 public class Stringdestroyer {
-    public static void stringdestroyer(String[] args) {
-        String line = "";
+    public static void stringdestroyer(String csv_rpath,PrintWriter pw,StringBuilder sb,StringBuilder in) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(csv_rpath));
+            String line = "";
+
+            while((line = br.readLine()) != null){
+
+                String[] value = line.split("(,|;)(?=(?:[^“]*“[^“]*“)*[^“]*$)");
+
+                for(int i = 0; i < value.length; i++) {
+
+                    if(value[i]==""){
+                        continue;
+                    }
+
+
+                    if(value[i].indexOf("“") == 0){
+                        value[i] = value[i].replaceFirst("“","");
+
+                        if(value[i].contains("““") == true){
+                            value[i] = value[i].substring(0,value[i].length()-1);
+                            value[i] = value[i].replaceFirst("““","“");
+                        }else if(value[i].indexOf("“") != value[i].length()-1){
+                            value[i] = value[i].replaceFirst("“","");
+                        }else{
+                            value[i] = value[i].replace(value[i].substring(value[i].length()-1),"");
+                        }
+
+
+                    }else if(value[i].contains("““") == true){
+
+                        value[i] = value[i].replaceFirst("““","“");
+
+                    }else if(value[i].indexOf("“") != 0 && value[i].contains("“") == true){
+                        String[] dopp = value[i].split(",");
+                        for(int a=0;a<dopp.length;a++){
+                            sb.append(dopp[a]+" ");
+                            in.append(dopp[a].length()+"+");
+                        }
+                        continue;
+
+
+                    }else{
+                        value[i] =value[i];
+                    }
+
+                    sb.append(value[i]+" ");
+                    in.append(value[i].length()+"+");
+
+                }
+
+                sb.append(in);
+                sb.deleteCharAt(sb.lastIndexOf("+"));
+                sb.append("\n");
+                in.delete(0, in.length());
+            }
+            System.out.println(sb.toString());
+            pw.write(sb.toString());
+            pw.close();
 
 
 
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
 
     }
 }
